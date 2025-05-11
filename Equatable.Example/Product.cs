1d0bdfdddcb02bae8452a962ef65ded4aeb622ac
iskandar <emomov.iskandar0110@gmail.com>
2025-05-11 13:12:50 +0200

    example added;

---
+++ Equatable.Example/Product.cs
@@ -0,0 +1,17 @@
+
+namespace Equatable.Example
+{
+    public class Product : BaseEquatable
+    {
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string? Category { get; set; }
+        public string? Description { get; set; }
+
+
+        public override List<object?> Props => [Name, Price, Category, Description];
+        public override bool? Stringify => true;
+    }
+
+}
--- Equatable.Example/Program.cs
+++ Equatable.Example/Program.cs
@@ -20,3 +20,22 @@ var person3 = new Person
 Console.WriteLine(person.Equals(person3)); // False
 
 Console.WriteLine(person); // John, 30
+
+var product = new Product
+{
+    Name = "Laptop",
+    Price = 999.99m,
+    Quantity = 10,
+    Category = "Electronics",
+    Description = "A high-performance laptop."
+};
+var product2 = new Product
+{
+    Name = "Laptop",
+    Price = 999.99m,
+    Quantity = 11,
+    Category = "Electronics",
+    Description = "A high-performance laptop."
+};
+//Even though the quantity is different, the Equals method will only check the properties defined in the Props list
+Console.WriteLine(product.Equals(product2)); // True
